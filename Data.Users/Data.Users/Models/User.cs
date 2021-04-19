f5a79c4fdb6be9dbf773c3719b6c2ee185aade48
Joao Gouveia <jevg@live.com.pt>
2021-04-19 19:55:21 +0100

    Users class name change

--- Data.Users/Data.Users/Models/User.cs
+++ Data.Users/Data.Users/Models/User.cs
@@ -3,7 +3,7 @@
     using System;
     using System.ComponentModel.DataAnnotations;
 
-    public class UserDAO
+    public class User
     {
         /// <summary>
         /// Id
--- Data.Users/Data.Users/UsersDbContext.cs
+++ Data.Users/Data.Users/UsersDbContext.cs
@@ -17,7 +17,7 @@ namespace Data.Users
         /// <summary>
         /// Users
         /// </summary>
-        public DbSet<UserDAO> Users { get; set; }
+        public DbSet<User> Users { get; set; }
 
         /// <summary>
         /// On configuring
@@ -34,11 +34,11 @@ namespace Data.Users
         /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<UserDAO>()
+            modelBuilder.Entity<User>()
                 .Property(x => x.CreateDate)
                 .ValueGeneratedOnAdd(); 
             
-            modelBuilder.Entity<UserDAO>()
+            modelBuilder.Entity<User>()
                 .Property(x => x.UpdateDate)
                 .ValueGeneratedOnUpdate();
         }
