0cb3f6e01bec8f8ba592cabe161cbdee3244e5d7
Joao Gouveia <jevg@live.com.pt>
2021-04-19 18:53:03 +0100

    Add project files.

---
+++ Data.Users/Data.Users/Models/UserDAO.cs
@@ -0,0 +1,60 @@
+﻿namespace Data.Users.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserDAO
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        [StringLength(50, ErrorMessage = "Max first name length is 50")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email must be valid")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(50, ErrorMessage = "Max password length is 50")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Status
+        /// <para>1: Active</para>
+        /// <para>0: Undefined</para>
+        /// <para>-1: Disabled</para>
+        /// </summary>
+        [Range(-1, 1, ErrorMessage = "Status must be between -1 and 1")]
+        public short Status { get; set; }
+
+        /// <summary>
+        /// Create date
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Update date
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+    }
+}
---
+++ Data.Users/Data.Users/UsersDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Data.Users.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data.Users
+{
+    public class UsersDbContext : DbContext
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public UsersDbContext(DbContextOptions<UsersDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Users
+        /// </summary>
+        public DbSet<UserDAO> Users { get; set; }
+
+        /// <summary>
+        /// On configuring
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Configs
+        }
+
+        /// <summary>
+        /// On Model configuration
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDAO>()
+                .Property(x => x.CreateDate)
+                .ValueGeneratedOnAdd(); 
+            
+            modelBuilder.Entity<UserDAO>()
+                .Property(x => x.UpdateDate)
+                .ValueGeneratedOnUpdate();
+        }
+    }
+}
