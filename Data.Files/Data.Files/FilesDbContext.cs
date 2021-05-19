aa45947da6da0410b4a536c2c4493838f8aaab32
Joao Gouveia <jevg@live.com.pt>
2021-05-19 22:34:34 +0100

    Inital Commit

---
+++ Data.Files/Data.Files/FilesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Data.Files.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data.Files
+{
+    public class FilesDbContext : DbContext
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public FilesDbContext(DbContextOptions<FilesDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<File> Files { get; set; }
+    }
+}
---
+++ Data.Files/Data.Files/Models/File.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Files.Models
+{
+    public class File
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Key
+        /// </summary>
+        public string Key { get; set; } = Guid.NewGuid().ToString().Replace("-", "").ToString();
+
+        /// <summary>
+        /// Original name
+        /// </summary>
+        public string OriginalName { get; set; }
+
+        /// <summary>
+        /// Content type
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// Extension
+        /// </summary>
+        public string Extension { get; set; }
+
+        /// <summary>
+        /// Path
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Create date
+        /// </summary>
+        public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// Update date
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+    }
+}
